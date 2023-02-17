99454f535ce529ae9d7c6d1e1e6fc3e497c69578
Jing <111>
2023-02-17 16:07:36 +0800

    123

---
+++ src/wj/ClickUtil.java
@@ -0,0 +1,21 @@
+package wj;
+
+public class ClickUtil {
+
+    private static final long MIN_DELAY_TIME = 3000L;
+    private static long lastClickTime;
+
+    public ClickUtil() {
+    }
+
+    public static boolean isFastClick() {
+        boolean flag = true;
+        long currentClickTime = System.currentTimeMillis();
+        if (currentClickTime - lastClickTime >= MIN_DELAY_TIME) {
+            flag = false;
+        }
+
+        lastClickTime = currentClickTime;
+        return flag;
+    }
+}
