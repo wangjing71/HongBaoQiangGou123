5189d87663d01e4be30107b963214f2269edea8e
Jing <111>
2023-02-20 09:29:55 +0800

    123

--- src/wj/MainPage.java
+++ src/wj/MainPage.java
@@ -9,15 +9,7 @@ import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static wj.QiangGouUtil.*;
 
