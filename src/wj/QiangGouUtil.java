a1e120ed25bb9b8048f3155b35b1f8f3584d7018
Jing <111>
2023-02-23 15:36:50 +0800

    123

--- src/wj/QiangGouUtil.java
+++ src/wj/QiangGouUtil.java
@@ -32,7 +32,7 @@ public class QiangGouUtil {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    ExecutorService pl = Executors.newFixedThreadPool(5);
+                    ExecutorService pl = Executors.newFixedThreadPool(7);
                     for (int j = 0; j < 200; j++) {
                         pl.execute(new Runnable() {
                             @Override
